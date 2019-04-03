0f44bafa1ea6b0fc138d00649236d3c98a063f85
AndrewLawler <AndrewLawler_@hotmail.com>
2019-04-03 12:42:11 +0100

    done

--- Compute1.java
+++ Compute1.java
@@ -64,10 +64,11 @@ class Apprentice extends Thread
          System.out.println("Apprentice " + id + " has retrieved a tablet. The number is " + value); 
          tot += value;
 
-         // if we are on last loop at the end, send the overall total to the insert method before yield
+         // if we are on the final loop (i==2) send total and id to insert before we yield
          if(i==2){
             b.insert(tot,id);
          }
+
          // Yield to allow fairer scheduling of threads
          Thread.yield();
       }
@@ -75,129 +76,92 @@ class Apprentice extends Thread
 
 }
 
-// Volumina Thread
 class Volumina extends Thread {
 
-   // Takes in box as parameter
+   // setting finalTotal to 0 so we can use it to calculate our running total, taking box as a parameter and also NUMAPPS
    private Box b;
    int finalTotal = 0;
+   int n;
 
-   // Constructor to take the box and assign b to box
-   public Volumina(Box box){
+   // show box being taken as parameter in the constructor as well as NUMAPPS, we then assign box to b and NUMAPPS to n
+   public Volumina(Box box, int NUMAPPS){
+      n = NUMAPPS;
       b = box;
    }
 
-   // run method
    public void run(){
-      // While finish condition is false, remove from the box and add to the final total
-      while(b.finish==false){
-         int n = b.remove();
-         finalTotal = finalTotal + n;
-      }  
-      // When over, summon the final total
-      System.out.println("Volumina summons the final total...");
-      System.out.println("Total is "+finalTotal);
-      
+      // while we dont have all the pages, run this
+      while(b.pagetotal<n){  
+         // add to our finalTotal
+         finalTotal = finalTotal + b.remove();
+      }
+      // print final outcome
+      System.out.println("\nVolumina has all the pages");
+      System.out.println("Final total is: "+finalTotal);
    } 
 
 }
 
 class Box {
 
-   // Initialize Linked List box
+   // initializing box and pagetotal counter
    LinkedList<Integer> box = new LinkedList<>();
-
-   // Initializing attributes including runningtotal and finish boolean
-   private int v;
-   int RunningTotal = 0;
-   boolean finish = false;
-   private int e;
-
-   // Constructor to assign endresult to e
-   public Box(int endResult){
-      e = endResult;
-   }
-
-   // Insert Method
-   public synchronized void insert(int value, int id) {
-     
-      // While box has two elements, wait
+   int pagetotal = 0;
+   // The box does not reference NUMCUBS or NUMAPPS, this is important as it means the project can scale without having to edit. 
+   // If you wanted to make the box bigger, just increment the while(box.size==2) to something different.
+   public synchronized void insert(int value, int id){
+      // while box = full, wait
       while(box.size()==2){
          try {
             wait();
          } 
          catch (InterruptedException e) {}
       }
-
-      // If box size is less than 2, add to the box
-      if(box.size()<2){
-         System.out.println("Apprentice " + id + " has moved their parchment with total "+ value + " into the box");
-         box.add(value);
-      }
-      
-      // Not finished
-      finish = false;
-
-      // Notify waiting threads
+      // add to box, print which value has been added and then print the box itself
+      box.add(value);
+      System.out.println("Apprentice " + id + " has moved their parchment with total "+ value + " into the box");
+      System.out.println("Box: "+box);
+      // notify waiting threads so they can try and enter their parchment again
       notify();
    }
    
-   // Remove Method
    public synchronized int remove() {
-
-      // While box is empty, wait
+      // while box = empty, wait
       while(box.size()==0) {
          try{
             wait();
          }
          catch (Exception e) {}
       }
-
-      // If box size is greater than or equal to one, remove the first element
-      if(box.size()>=1){
-         v = box.removeFirst();
-      }
-
+      // remove first value in box and print out its value, also print out the box itself
+      int v = box.removeFirst();
       System.out.println("Volumina removed a total of "+v+" from the box");
-
-      // add v(total removed) to the runningtotal
-      RunningTotal = RunningTotal+v;
-
-      // check is running total is equal to the end result calculated. If so, change finish to true
-      if(RunningTotal==e){
-         finish = true;
-      }
-
-      // Notify threads and return v to Volumina
+      System.out.println("Box: "+box);
+      // add to page total and then print out the current amount of pages we have
+      pagetotal++;
+      System.out.println("Volumina Page Count: " + pagetotal);
       notify();
       return v;
-      
    }
    
 }
 
 
-public class Compute1 
+public class Compute1
 {
    // We will have 18 cubicles and 6 Apprentices
+   // My program is scalable as i could simply change this NUMCUBS value and it would still work perfectly.
    private static final int NUMCUBS = 18;
+   // same for NUMAPPS, i passed NUMAPPS into my volumina thread so if you scale up it would still work perfectly. I would just have to make sure that NUMCUBS/NUMAPPS = 3
    private static final int NUMAPPS = 6;
 
    public static void main(String args[]) 
    {  
-
-      // Finding end result
-      // eg: All cubicles added
-      int endResult = 0;
-      for(int i=1; i<=NUMCUBS; i++){
-         endResult = endResult+i;
-      }
+      // creating new box object
+      Box box = new Box();
       
-      // Sending endresult to box
-      Box box = new Box(endResult);
-
-      // Passing volumina into box
-      Volumina v = new Volumina(box);
+      // creating volumina and then running her (key: passing box in as parameter and also NUMAPPS so the project is scalable)
+      Volumina v = new Volumina(box, NUMAPPS);
       v.start();
 
       // Set up array of cubicles. Have an extra one so that we can index from 1.
@@ -206,7 +170,7 @@ public class Compute1
       for (int i = 1; i <= NUMCUBS; i++)
          row[i] = new Cubicle(i);
 
-      // Set up array of Apprentices
+      // Set up array of Apprentices (key: passing box in as parameter)
       Apprentice apprentices[] = new Apprentice[NUMAPPS];
       for (int i = 0; i < NUMAPPS; i++)
          apprentices[i] = new Apprentice(row, i+1, box);
